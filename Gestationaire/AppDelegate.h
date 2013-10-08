cbaecd4ade15bef227f830bb00004f89bffcc318
admin <admin@imac-125b1b.formation.m2i>
2013-10-08 11:28:29 +0200

    Initial Commit

---
+++ Gestationaire/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Gestationaire
+//
+//  Created by admin on 08/10/13.
+//  Copyright (c) 2013 Aurelien Fontaine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Gestationaire/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Gestationaire
+//
+//  Created by admin on 08/10/13.
+//  Copyright (c) 2013 Aurelien Fontaine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
