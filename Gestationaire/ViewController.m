5b260ec4b2b3d40c7fce378b72ce62d19d3cc1be
admin <admin@imac-125b1b.formation.m2i>
2013-10-08 16:39:36 +0200

    Add fun effect

--- Gestationaire/ViewController.m
+++ Gestationaire/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 @property (strong, nonatomic) UIImageView* imgview;
+@property (strong, nonatomic) UIImageView* trashOpenView;
+@property (strong, nonatomic) UIImageView* trashCloseView;
 @end
 
 @implementation ViewController
@@ -20,25 +22,41 @@
     
     self.view.backgroundColor = [UIColor whiteColor];
     
-    UIImage *img = [UIImage imageNamed:@"g"];
+    [self addImage];
+    
+    
+    UIImage *trashClose = [UIImage imageNamed:@"Trash_Close"];
+    UIImage *trashOpen = [UIImage imageNamed:@"Trash_Open"];
+    
+    CGRect trashPos = CGRectMake(0,350,trashClose.size.width/3, trashClose.size.height/3);
 
-    CGFloat w = img.size.width;
-    CGFloat h = img.size.height;
+    self.trashOpenView = [[UIImageView alloc] initWithImage:trashOpen];
+    self.trashCloseView = [[UIImageView alloc] initWithImage:trashClose];
     
-    CGRect rect = CGRectMake(0,0,w,h);
+    self.trashCloseView.frame = trashPos;
+    self.trashOpenView.frame = trashPos;
     
-    CIContext *context = [CIContext contextWithOptions:nil];
-    CIImage *image = img.CIImage;
     
-    CIFilter *filter = [CIFilter filterWithName:@"CISepiaTone"];
-    [filter setValue:image forKey:kCIInputImageKey];
-    [filter setValue:[NSNumber numberWithFloat:0.8f] forKey:@"InputIntensity"];
-    CIImage *result = [filter valueForKey:kCIOutputImageKey]; // 4
-    CGImageRef cgImage = [context createCGImage:result fromRect:rect];
+    self.trashOpenView.alpha = 0;
     
-    UIImage *imgFiltered = [UIImage imageWithCGImage:cgImage];
+    [self.view addSubview:self.trashCloseView];
+    [self.view addSubview:self.trashOpenView];
     
-    self.imgview = [[UIImageView alloc] initWithImage:imgFiltered];
+    
+    UIButton* btn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    btn.frame = CGRectMake(250, 0, 50, 50);
+    [btn setTitle:@"+" forState:UIControlStateNormal];
+    
+    [btn addTarget:self action:@selector(addImage) forControlEvents:UIControlEventTouchDown];
+    
+    [self.view addSubview:btn];
+}
+
+-(void) addImage
+{
+    UIImage *img = [UIImage imageNamed:@"g"];
+    
+    self.imgview = [[UIImageView alloc] initWithImage:img];
     self.imgview.userInteractionEnabled = YES;
     [self.view addSubview:self.imgview];
     
@@ -49,23 +67,44 @@
     
     tap.numberOfTapsRequired = 2;
     
+    pinch.delegate = self;
+    rotate.delegate = self;
+    
     [self.imgview addGestureRecognizer:move];
     [self.imgview addGestureRecognizer:tap];
     [self.imgview addGestureRecognizer:pinch];
     [self.imgview addGestureRecognizer:rotate];
+
+}
+
+-(BOOL) gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer
+{
+    return YES;
 }
 
 -(void) handleRotate:(UIRotationGestureRecognizer *)gest
 {
-    gest.view.transform = CGAffineTransformRotate(gesture.view.transform, gest.rotation);
+    gest.view.transform = CGAffineTransformRotate(gest.view.transform, gest.rotation);
     gest.rotation = 0;
 }
 
 
--(void) handleMove:(UIGestureRecognizer *)gest
+-(void) handleMove:(UIPanGestureRecognizer *)gest
 {
-    CGPoint location = [gest locationInView:self.view];
-    gest.view.center = location;
+    CGPoint location = [gest translationInView:self.view];
+    
+    
+    CGFloat x = gest.view.center.x +location.x;
+    CGFloat y = gest.view.center.y + location.y;
+    
+    gest.view.center = CGPointMake(x, y);
+
+    
+    if(gest.state == UIGestureRecognizerStateEnded && CGRectContainsPoint(self.trashCloseView.frame, gest.view.center)) {
+        [self moveToTrash:gest];
+    }
+    
+    [gest setTranslation:CGPointMake(0,0) inView:self.view];
 }
 
 -(void) handlePinch:(UIPinchGestureRecognizer *)gesture
@@ -75,12 +114,59 @@
    
 }
 
--(void) handleTap:(UISwipeGestureRecognizer *)gest
+- (id)finishRemoveImage:(UIGestureRecognizer *)gest
 {
-    [UIView animateWithDuration:1 animations:^{
-        gest.view.center = self.view.center;
-    }];
+    return ^(BOOL finished) {
+        
+        if(finished) {
+            [gest.view removeFromSuperview];
+        }
+    };
 }
 
+- (void)moveToTrash:(UIGestureRecognizer *)gest
+{
+    [UIView animateWithDuration:0.5 animations:^{
+        self.trashCloseView.alpha = 0;
+        self.trashOpenView.alpha = 1;
+        
+        CGPoint point = [gest locationInView:self.view];
+        
+        CGFloat x = point.x - self.trashCloseView.center.x;
+        CGFloat y = self.trashCloseView.center.y - point.y;
+        
+        CGFloat angle =  atanf(x/y);
+        
+        if(y < 0) {
+            angle = angle + 180;
+        }
+        
+        NSLog(@" x %f- y %f - angle %f",x, y, angle);
+        
+        
+        [UIView animateWithDuration:4 animations:^{
+            
+            self.trashCloseView.transform = CGAffineTransformMakeRotation(angle);
+            self.trashOpenView.transform = CGAffineTransformMakeRotation(angle);
+            gest.view.transform = CGAffineTransformScale(gest.view.transform, 0, 0);
+            gest.view.center = self.trashCloseView.center;
+            
+
+            
+        } completion:^(BOOL finished) {
+            [UIView animateWithDuration:1 animations:^{
+                self.trashCloseView.transform = CGAffineTransformMakeRotation(0);
+                self.trashOpenView.transform = CGAffineTransformMakeRotation(0);
+                    self.trashCloseView.alpha = 1;
+                    self.trashOpenView.alpha = 0;
+                } completion:[self finishRemoveImage:gest]];
+            }];    
+        }];
+}
+
+-(void) handleTap:(UIGestureRecognizer *)gest
+{    
+    [self moveToTrash:gest];
+}
 
 @end
